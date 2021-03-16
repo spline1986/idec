857987783b79114b47bdca83169419e56282c121
Andrew Lobanov <spline1986@yandex.ru>
2021-03-16 13:06:09 +0500

    Fetcher can use different configuration files.

--- fetcher.py
+++ fetcher.py
@@ -1,10 +1,21 @@
 import json
+import sys
 from client import Client
 from sqlite import Sqlite
+from typing import Dict
 from uplink import Uplink
 
-config = json.loads(open("fetcher.json").read())
-base = Sqlite(config["base"])
-uplink = Uplink(config["uplink"])
-client = Client(uplink, base, config["echoareas"])
-print(client.download_mail(), "messages downloaded.")
+
+def load_config(filename: str = "fetcher.json") -> Dict:
+    return json.loads(open(filename).read())
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        config = load_config(sys.argv[1])
+    else:
+        config = load_config()
+    base = Sqlite(config["base"])
+    uplink = Uplink(config["uplink"])
+    client = Client(uplink, base, config["echoareas"])
+    print(client.download_mail(), "messages downloaded.")
